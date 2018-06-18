857a5778e01a0079f16221e8c61c8f5f354587ec
Marius Gripsgard <marius@ubports.com>
2018-06-18 22:00:35 +0200

    TEST

--- src/aalimagecapturecontrol.cpp
+++ src/aalimagecapturecontrol.cpp
@@ -171,6 +171,7 @@ void AalImageCaptureControl::saveJpeg(const QByteArray& data)
 
     // Restart the viewfinder and notify that the camera is ready to capture again
     if (m_service->androidControl()) {
+	android_camera_stop_preview(m_service->androidControl());
         android_camera_start_preview(m_service->androidControl());
     }
     m_service->updateCaptureReady();
@@ -187,12 +188,6 @@ void AalImageCaptureControl::saveJpeg(const QByteArray& data)
 void AalImageCaptureControl::onImageFileSaved()
 {
     DiskWriteWatcher* watcher = static_cast<DiskWriteWatcher*>(sender());
-
-    // Restart the viewfinder and notify that the camera is ready to capture again
-    if (m_service->androidControl()) {
-        android_camera_start_preview(m_service->androidControl());
-    }
-
 
     if (m_pendingSaveOperations.contains(watcher)) {
         int requestID = m_pendingSaveOperations.take(watcher);
